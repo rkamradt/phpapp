a05be23425d9f7a2dcc277bd3a747fd82b78d47f
Randal Kamradt <randykamradt@gmail.com>
2020-09-20 13:48:02 -0700

    added some pages and links between pages

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -7,11 +7,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class Home extends AbstractController
 {
-    public function index(): Response
-    {
-        $number = random_int(0, 100);
-        return $this->render('index.html.twig', [
-            'number' => $number,
-        ]);
-    }
+  public function index(): Response
+  {
+      $number = random_int(0, 100);
+      return $this->render('home.html.twig', [
+          'number' => $number,
+      ]);
+  }
+  public function list(): Response
+  {
+      $number = random_int(0, 100);
+      return $this->render('list.html.twig', [
+          'number' => $number,
+      ]);
+  }
+  public function about(): Response
+  {
+      $number = random_int(0, 100);
+      return $this->render('about.html.twig', [
+          'number' => $number,
+      ]);
+  }
 }
